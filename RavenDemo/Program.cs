defbb8d44b1917507d6193c9ea543d40923b1ad8
juanonsoftware <juanonsoftware@gmail.com>
2020-09-24 16:57:48 +0700

    Session 2 - how to implement search function

--- RavenDemo/Program.cs
+++ RavenDemo/Program.cs
@@ -3,6 +3,7 @@ using Raven.Client.Document;
 using RavenDemo.Domain;
 using System;
 using System.Configuration;
+using System.Linq;
 
 namespace RavenDemo
 {
@@ -10,36 +11,40 @@ namespace RavenDemo
     {
         static void Main(string[] args)
         {
-            int selection = 0;
+            var selection = 0;
             do
             {
-                selection = ShowMenu();
+                int.TryParse(ShowMenu(), out selection);
+
                 if (selection == 1)
                 {
                     InitializeSampleData();
                 }
 
+                if (selection == 2)
+                {
+                    SearchProducts();
+                }
 
-
-            } while (selection > 0);
+            } while (selection >= 0);
         }
 
-        private static int ShowMenu()
+        private static string ShowMenu()
         {
             Console.WriteLine(
                 "---" + Environment.NewLine +
                 "Select your option:" + Environment.NewLine +
                 "1: Initialize sample data" + Environment.NewLine +
-                "2: " + Environment.NewLine +
-                "0: Exit"
+                "2: Search for products" + Environment.NewLine +
+                "-1: Exit"
             );
 
-            return int.Parse(Console.ReadLine().Trim());
+            return Console.ReadLine();
         }
 
         private static void InitializeSampleData()
         {
-            var products = DomainUtils.BuildSampleProducts(100);
+            var products = DomainUtils.BuildSampleProducts(150);
 
             using (var store = CreateStore())
             {
@@ -55,6 +60,50 @@ namespace RavenDemo
             }
 
             Console.WriteLine("Documents saved");
+        }
+
+        private static void SearchProducts()
+        {
+            Console.WriteLine("Enter your text to search");
+            var text = Console.ReadLine().Trim();
+
+            var products = Enumerable.Empty<Product>();
+
+            using (var store = CreateStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var productsQuery = session.Query<Product>()
+                        .Customize(c => c.BeforeQueryExecution(a => a.PageSize = 20));
+
+                    if (string.IsNullOrEmpty(text))
+                    {
+                        products = productsQuery.ToList();
+                    }
+                    else
+                    {
+                        // this doesn't work for searching substring
+                        //products = productsQuery.Where(p => p.Name.Contains(text)).ToList();
+
+                        // Search by AND option
+                        //products = productsQuery
+                        //    .Search(p => p.Name, text, escapeQueryOptions: EscapeQueryOptions.RawQuery)
+                        //    .ToList();
+
+                        // Adding boost on Name field to prefer that than Intro field
+                        products = productsQuery
+                            .Search(p => p.Name, text, escapeQueryOptions: EscapeQueryOptions.AllowAllWildcards, boost: 10)
+                            .Search(p => p.Intro, text, options: SearchOptions.Or, escapeQueryOptions: EscapeQueryOptions.AllowAllWildcards)
+                            .ToList();
+                    }
+                }
+            }
+
+            Console.WriteLine("Results of the search: {0} items", products.Count());
+            foreach (var p in products)
+            {
+                Console.WriteLine("Id: {0} \tName: {1}", p.Id, p.Name);
+            }
         }
 
         private static IDocumentStore CreateStore()
